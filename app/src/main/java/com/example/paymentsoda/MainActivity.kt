0ddb561ebf1f3f6e4046ac61e5f5e114f90811ab
syoma <skat.apps@gmail.com>
2022-07-29 22:17:20 +0300

    realization topBar

--- app/src/main/java/com/example/paymentsoda/MainActivity.kt
+++ app/src/main/java/com/example/paymentsoda/MainActivity.kt
@@ -4,23 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.paymentsoda.ui.navigation.MyNavHost
+import com.example.paymentsoda.ui.theme.Blue500
 import com.example.paymentsoda.ui.theme.PaymentSodaTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PaymentSodaTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+
+                Scaffold(
+                    topBar = {
+
+                        val systemUI = rememberSystemUiController()
+                        systemUI.isStatusBarVisible = false
+
+                        Button(onClick = { navController.popBackStack() }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_back),
+                                contentDescription = "back"
+                            )
+                        }
+                    }
                 ) {
+                    MyNavHost(navContoller = navController)
 
                 }
             }
