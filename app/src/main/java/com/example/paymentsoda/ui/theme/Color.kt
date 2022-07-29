ac1ed9c7c593428409df053c0983633e95f307d5
syoma <skat.apps@gmail.com>
2022-07-29 14:28:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/paymentsoda/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.example.paymentsoda.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue200 = Color(0xFF5EC4FF)
+val Blue500 = Color(0xFF0C96E4)
+val Blue700 = Color(0xFF3558D5)
+
+val Teal200 = Color(0xFFECFEFC)
+val Teal500 = Color(0xFF67D4C7)
+
+val Gray200 = Color(0xFFF6F6F8)
+val Gray500 = Color(0xFF475062)
+
+val Yellow200 = Color(0xFFFFD260)
+val Yellow500 = Color(0xFFFFBD12)
+
+val Red200 = Color(0xFFFF83A1)
+val Red500 = Color(0xFFF66A8C)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
