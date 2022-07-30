1c73ec98f50c6314a23604db5828a2693e77e903
syoma <skat.apps@gmail.com>
2022-07-30 19:22:40 +0300

    create ButtonProcessedForPayment

--- app/src/main/java/com/example/paymentsoda/MainActivity.kt
+++ app/src/main/java/com/example/paymentsoda/MainActivity.kt
@@ -7,20 +7,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.example.paymentsoda.ui.navigation.MyNavHost
 import com.example.paymentsoda.ui.navigation.Screens
 import com.example.paymentsoda.ui.theme.Blue500
+import com.example.paymentsoda.ui.theme.Gray500
 import com.example.paymentsoda.ui.theme.PaymentSodaTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -38,7 +43,10 @@ class MainActivity : ComponentActivity() {
                         val systemUI = rememberSystemUiController()
                         systemUI.isStatusBarVisible = false
 
-                        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                        Row(
+                            Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
                             Button(onClick = { navController.popBackStack() }) {
                                 Icon(
                                     painter = painterResource(id = R.drawable.ic_back),
@@ -50,7 +58,9 @@ class MainActivity : ComponentActivity() {
 
                                 Text(
                                     text = title.value,
-                                    modifier = Modifier.fillMaxWidth().weight(1f),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f),
                                     fontSize = 18.sp,
                                     textAlign = TextAlign.Center,
 
@@ -63,5 +73,22 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ButtonProcessedForPayment(onClick: () -> Unit) {
+    Button(
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(Gray500),
+        modifier = Modifier.padding(top = 32.dp)
+    ) {
+        Text(
+            text = "Processed for Payment",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)
+        )
     }
 }
