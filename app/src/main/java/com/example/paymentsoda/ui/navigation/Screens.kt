2ee6ba80e2cef5649f396ade6f0d5b3702be5579
syoma <skat.apps@gmail.com>
2022-07-30 17:39:13 +0300

    create navController

--- app/src/main/java/com/example/paymentsoda/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/paymentsoda/ui/navigation/Screens.kt
@@ -2,7 +2,7 @@ package com.example.paymentsoda.ui.navigation
 
 sealed class Screens(val route: String, val name: String = "") {
     object ProductSelection : Screens("productSelection")
-    object Cart : Screens("cart")
+    object Cart : Screens("cart", "Cart")
     object SelectAddress : Screens("selectAddress", "Select your deliver address")
     object Payment : Screens("payment", "Select your payment options")
     object SelectedCard : Screens("selectedCard")
