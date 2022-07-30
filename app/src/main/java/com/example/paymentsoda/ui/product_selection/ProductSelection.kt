e63ea74953ead968499e990f11d87867ec778535
syoma <skat.apps@gmail.com>
2022-07-30 16:53:02 +0300

    realization infrormation

--- app/src/main/java/com/example/paymentsoda/ui/product_selection/ProductSelection.kt
+++ app/src/main/java/com/example/paymentsoda/ui/product_selection/ProductSelection.kt
@@ -5,57 +5,95 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.paymentsoda.R
-import com.example.paymentsoda.data.Soda
-import com.example.paymentsoda.data.sodaTestList
+import com.example.paymentsoda.data.*
 import com.example.paymentsoda.ui.Title
 import com.example.paymentsoda.ui.theme.Gray200
 import com.example.paymentsoda.ui.theme.Gray500
-import com.example.paymentsoda.ui.theme.Teal200
 import com.example.paymentsoda.ui.theme.Teal500
 
 @Composable
 fun ProductSelection(navContoller: NavHostController) {
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         ProductSelectionTitle()
-
-
-        val checkedProducts = remember { mutableListOf<Soda>() }
-
-        LazyRow(modifier = Modifier.padding(top = 16.dp)) {
-            items(sodaTestList) { product ->
-
-                val check = remember { mutableStateOf(checkedProducts.contains(product))}
-
-                ItemProduct(
-                    product = product,
-                    isCheck = check.value,
-                    checked = {
-                        if (check.value) {
-                            checkedProducts.remove(product)
-                        }else {
-                            checkedProducts.add(product)
-                        }
-                        check.value = check.value.not()
-                    }
-
-
-                )
-            }
-        }
-
+        LazyRowProducts()
+        InformationTable()
+        AllPrice()
     }
+}
 
+@Composable
+private fun AllPrice() {
+    Text(text = "$${allPrice.value}", style = MaterialTheme.typography.h3, modifier = Modifier.padding(top = 36.dp))
+}
+
+
+@Composable
+private fun InformationTable() {
+    Row(
+        modifier = Modifier
+            .padding(top = 8.dp)
+            .background(color = Gray200, shape = RoundedCornerShape(32.dp))
+            .padding(vertical = 24.dp, horizontal = 16.dp)
+            .width(250.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Quantity", color = Color.Gray)
+
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_bottle),
+                contentDescription = "bottle",
+                tint = Gray500,
+                modifier = Modifier.size(20.dp)
+            )
+
+            Text("${sizeCheckedProducts.value} Bottle", color = Gray500)
+        }
+    }
+}
+
+@Composable
+private fun LazyRowProducts() {
+
+    LazyRow(modifier = Modifier.padding(top = 16.dp)) {
+        items(sodaTestList) { product ->
+
+            val check = remember { mutableStateOf(checkedProducts.contains(product)) }
+
+            ItemProduct(
+                product = product,
+                isCheck = check.value,
+                checked = {
+                    if (check.value) {
+                        checkedProducts.remove(product)
+                        sizeCheckedProducts.value = sizeCheckedProducts.value - 1
+                        allPrice.value = allPrice.value - product.price
+                    } else {
+                        checkedProducts.add(product)
+                        sizeCheckedProducts.value = sizeCheckedProducts.value + 1
+                        allPrice.value = allPrice.value + product.price
+                    }
+                    check.value = check.value.not()
+                }
+
+
+            )
+        }
+    }
 }
 
 @Composable
@@ -75,10 +113,12 @@ private fun ItemProduct(product: Soda, isCheck: Boolean, checked: () -> Unit) {
         Image(
             painter = painterResource(id = product.imageId),
             contentDescription = product.title,
-            modifier = Modifier.fillMaxSize().weight(1f)
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
         )
 
-        TextButton(onClick = { checked() } ) {
+        TextButton(onClick = { checked() }) {
             Icon(
                 tint = if (isCheck) Teal500 else Gray500,
                 painter = (painterResource(id = if (isCheck) R.drawable.ic_check_mark else R.drawable.ic_add)),
@@ -90,7 +130,6 @@ private fun ItemProduct(product: Soda, isCheck: Boolean, checked: () -> Unit) {
     }
 
 }
-
 
 @Composable
 private fun ProductSelectionTitle() {
