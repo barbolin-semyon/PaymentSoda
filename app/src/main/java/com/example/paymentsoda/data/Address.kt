0823cf4057d498062bf20ec3aec01471c3b2e254
syoma <skat.apps@gmail.com>
2022-07-30 20:51:59 +0300

    change Address.kt (delete data class)

--- app/src/main/java/com/example/paymentsoda/data/Address.kt
+++ app/src/main/java/com/example/paymentsoda/data/Address.kt
@@ -2,10 +2,6 @@ package com.example.paymentsoda.data
 
 import androidx.compose.runtime.mutableStateOf
 
-data class Address(
-    val text: String
-)
-
 val testListAddress = mutableStateOf(listOf(
     "649 Penn Street Retdfort, MI 48239",
     "649 Penn Street Retdfort, MI 48239",
