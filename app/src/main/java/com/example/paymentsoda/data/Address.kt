496a64dbc7cb327a6ef6c35f01aba45e06d84d30
syoma <skat.apps@gmail.com>
2022-07-31 19:56:12 +0300

    change test address

--- app/src/main/java/com/example/paymentsoda/data/Address.kt
+++ app/src/main/java/com/example/paymentsoda/data/Address.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.mutableStateOf
 
 val testListAddress = mutableStateOf(listOf(
     "649 Penn Street Retdfort, MI 48239",
-    "649 Penn Street Retdfort, MI 48239",
-    "649 Penn Street Retdfort, MI 48239",
-    "649 Penn Street Retdfort, MI 48239",
-    "649 Penn Street Retdfort, MI 48239",
+    "650 Penn Street Retdfort, MI 48239",
+    "651 Penn Street Retdfort, MI 48239",
+    "652 Penn Street Retdfort, MI 48239",
+    "653_ Penn Street Retdfort, MI 48239",
 ))
