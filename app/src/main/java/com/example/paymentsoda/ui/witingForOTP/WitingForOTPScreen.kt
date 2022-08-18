e281e23e4cfe0c0aebfaa06f293dd463a7f3e19c
syoma <skat.apps@gmail.com>
2022-08-18 19:30:24 +0300

    create Button Send code OTP

--- app/src/main/java/com/example/paymentsoda/ui/witingForOTP/WitingForOTPScreen.kt
+++ app/src/main/java/com/example/paymentsoda/ui/witingForOTP/WitingForOTPScreen.kt
@@ -1,12 +1,16 @@
 package com.example.paymentsoda.ui.witingForOTP
 
-import android.os.Bundle
-import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextRange
@@ -22,8 +27,15 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.paymentsoda.ui.navigation.Screens
+import com.example.paymentsoda.ui.paymentResult.CircleTimer
+import com.example.paymentsoda.ui.paymentResult.TextViewCode
+import com.example.paymentsoda.ui.theme.Alpha
+import com.example.paymentsoda.ui.theme.Gray200
+import com.example.paymentsoda.ui.theme.Gray500
+import com.example.paymentsoda.ui.theme.White
 
 @Composable
 fun WitingForOTPScreen(navController: NavController) {
@@ -37,72 +49,60 @@ fun WitingForOTPScreen(navController: NavController) {
             navController.currentBackStackEntry?.arguments?.putString("code", it)
             navController.navigate(Screens.PaymentResult.route)
         }
-    }
-}
+        val isReverse = remember { mutableStateOf(false) }
+        val resendOTPisVisible = remember { mutableStateOf(true) }
 
-@Composable
-fun TextViewCode(
-    sizeCode: Int,
-    modifier: Modifier,
-    sizeField: Dp,
-    shape: Shape,
-    onWritedCode: (code: String) -> Unit,
-) {
-    val localFocusManager = LocalFocusManager.current
-
-    val writeCode = remember { mutableStateOf(" ".repeat(sizeCode)) }
-
-    Row(modifier = modifier) {
-        repeat(sizeCode) { index ->
-            OutlinedTextField(
-                value = TextFieldValue(
-                    text = writeCode.value[index].toString(),
-                    selection = TextRange(writeCode.value.lastIndex)
-                ),
-                onValueChange = { newValue ->
-                    if (newValue.text == "") {
-                        if (index > 0) {
-                            writeCode.value = writeCode.value.replaceIndex(index - 1, " ")
-                            localFocusManager.moveFocus(FocusDirection.Left)
-                        }
-                    } else {
-                        writeCode.value =
-                            writeCode.value.replaceIndex(index, newValue.text.replace(" ", ""))
-
-                        if (index + 1 < sizeCode) {
-                            writeCode.value = writeCode.value.replaceIndex(index + 1, " ")
-                            localFocusManager.moveFocus(FocusDirection.Right)
-                        } else {
-                            onWritedCode(writeCode.value)
-                        }
-
-                    }
-                },
-                keyboardActions = KeyboardActions(onPrevious = {
-                    localFocusManager.moveFocus(
-                        FocusDirection.Right
-                    )
-
-                }),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Previous
-                ),
-                shape = shape,
-                modifier = Modifier
-                    .size(sizeField)
-                    .padding(8.dp)
-            )
+        CircleTimer(isReverse = isReverse) {
+            resendOTPisVisible.value = true
+        }
+        ButtonSendOTP(resendOtpIsVisible = resendOTPisVisible.value) {
+            isReverse.value = isReverse.value.not()
+            resendOTPisVisible.value = false
         }
     }
 }
 
-fun String.replaceIndex(index: Int, char: String): String {
+@Composable
+fun ButtonSendOTP(resendOtpIsVisible: Boolean, onClick: () -> Unit,) {
+    if (resendOtpIsVisible) {
+        SpacerForButtonSendOTP()
 
-    return if (index < this.lastIndex) {
-        "${this.substring(0, index)}$char${this.substring(index + 1, this.length)}"
-    } else {
-        "${this.substring(0, index)}$char"
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Send OTP code",
+                fontSize = 18.sp,
+                modifier = Modifier.padding(end = 16.dp),
+                color = Gray500
+            )
+            Button(
+                onClick = { onClick() },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = White,
+                ),
+                border = BorderStroke(width = 1.dp, color = Color.Gray)
+            ) {
+                Text("Send", modifier = Modifier.padding(horizontal = 16.dp), color = Gray500)
+            }
+        }
+
+
+        SpacerForButtonSendOTP()
     }
+}
+
+@Composable
+fun SpacerForButtonSendOTP() {
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color.Gray)
+    )
 
 }
